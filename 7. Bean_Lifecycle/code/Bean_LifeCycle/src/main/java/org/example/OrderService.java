84840d6d41d7bdb0f3122589a105c85b47c7f26a
akshattjain <akshatjain2545@gmail.com>
2026-08-18 22:41:39 +0530

    .

---
+++ 7. Bean_Lifecycle/code/Bean_LifeCycle/src/main/java/org/example/OrderService.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+import java.security.PublicKey;
+
+@Component
+public class OrderService {
+
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService){
+        this.paymentService=paymentService;
+    }
+
+    public void placeOrder(){
+        System.out.println("Order placed");
+
+        paymentService.pay();
+    }
+}
