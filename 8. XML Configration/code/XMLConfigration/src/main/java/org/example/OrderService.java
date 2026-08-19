622003c567ca0a7127a5a3583eefdff3f0da05ff
akshattjain <akshatjain2545@gmail.com>
2026-08-19 22:49:46 +0530

    .

---
+++ 8. XML Configration/code/XMLConfigration/src/main/java/org/example/OrderService.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+
+public class OrderService {
+
+    public void placeOrder(){
+        System.out.println("Order Placed");
+    }
+}
