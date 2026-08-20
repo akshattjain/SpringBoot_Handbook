f5978860ff6cb8c5a38faf3928cf1920e4ec0407
akshattjain <akshatjain2545@gmail.com>
2026-08-21 00:05:12 +0530

    SpringBoot auto configrations

--- 9. SpringBoot Core/code/SpringBoot_Core/src/main/java/org/example/Main.java
+++ 9. SpringBoot Core/code/SpringBoot_Core/src/main/java/org/example/Main.java
@@ -1,9 +1,21 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+
+        ApplicationContext context =
+                SpringApplication.run(Main.class,args);
+
+        OrderService orderService=  context.getBean(OrderService.class);
+        orderService.placeOrder();
+
 
     }
 }
