f6592d28de4a7c69a47d58a10c7ace2a6ba85dc4
akshattjain <akshatjain2545@gmail.com>
2026-08-23 05:07:44 +0530

    notes

--- 10. SpringBoot_application/code/demo/src/main/java/in/strikes/demo/PaymentGateway.java
+++ 10. SpringBoot_application/code/demo/src/main/java/in/strikes/demo/PaymentGateway.java
@@ -1,4 +1,34 @@
 package in.strikes.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PaymentGateway {
+    @Value("${paymentGateway.type:RazorPay}")
+    private String type;
+
+    @Value("${paymentGateway.retry-count:3}")
+    private int retryCount;
+
+//    public PaymentGateway(@Value("${paymentGateway.type}") String type,@Value("${paymentGateway.retry-count}") int retryCount) {
+//        this.type = type;
+//        this.retryCount = retryCount;
+//    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getRetryCount() {
+        return retryCount;
+    }
+
+    public void setRetryCount(int retryCount) {
+        this.retryCount = retryCount;
+    }
 }
